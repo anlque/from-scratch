27a3192809d50e9984d2e44d5dc61214f054bb75
@an_crr <a.zhuravlova@gurukami.com>
2023-05-28 15:10:10 +0400

    first commit

---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,11 @@
+import { lazy } from "react";
+
+// import("./MainPage")
+
+export const MainPageAsync = lazy(
+  () =>
+    new Promise((resolve) => {
+      // @ts-ignore
+      setTimeout(() => resolve(import("./MainPage")), 1500);
+    })
+);
