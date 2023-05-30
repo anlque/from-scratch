bcf5ae0c74278c482149bb9109aa9e2ad57e4662
@an_crr <a.zhuravlova@gurukami.com>
2023-05-30 11:52:16 +0400

    ignore, cls, theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
+import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
